d16c5a147753bd215e35ce8e21eb0bb8ba1831d7
codex4jet <dipak01911@gmail.com>
2026-01-30 21:15:55 +0530

    Adding UI

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 int main(){
-    
+    printf("\n=====================================\n");
+printf("           ATM MAIN MENU              \n");
+printf("=====================================\n");
+printf("1. Balance Inquiry\n");
+printf("2. Cash Withdrawal\n");
+printf("3. Deposit Money\n");
+printf("4. Mini Statement\n");
+printf("5. Change PIN\n");
+printf("0. Exit\n");
+printf("-------------------------------------\n");
+printf("Enter your choice: ");
+
+
 
     return 0;
 }
