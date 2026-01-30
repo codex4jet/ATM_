5e7a95d8e8bb3c83bde5d448e012b62bd0bc5db1
Jinesh248 <jinesh.patel248@gmail.com>
2026-01-30 23:41:29 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int num,pin,amt,001,002,003,004,005;
+
+//pin and and atm  card no are same 
+
 int main(){
     system("cls");
     
@@ -9,7 +13,8 @@ int main(){
     printf("\n=====================================\n");
 printf("          WELCOME TO ATM             \n");
 printf("=====================================\n");
-printf("       Insert your ATM Card          \n");
+printf("       Insert your ATM Card Number          \n");
+scanf("%d",&num);
 printf("        (Press Enter)                \n");
 
 
@@ -25,7 +30,101 @@ printf("4. Mini Statement\n");
 printf("5. Change PIN\n");
 printf("0. Exit\n");
 printf("-------------------------------------\n");
+int ch;
 printf("Enter your choice: ");
+scanf("%d",&ch);
+
+    //Balance check
+if(ch == 1 && num == 001){
+    printf("Your current balance is %d Rs",5);
+}
+else-if(ch == 1 && num == 002){
+    printf("Your current balance is %d Rs",50);
+}
+else-if(ch == 1 && num == 003){
+    printf("Your current balance is %d Rs",25);
+}
+else-if(ch == 1 && num == 004){
+    printf("Your current balance is %d Rs",35);
+}
+else-if(ch == 1 && num == 005){
+    printf("Your current balance is %d Rs",95);
+}
+
+
+    //Withdrawal amount
+else-if(ch == 2 && num == 001){
+    printf("Enter you PIN: ");
+    scanf("%d",&pin);
+    if(pin == num){
+        printf("Enter the amount: ");
+        scanf("%d",&amt);
+        printf("Please wait patiently....");
+        printf("Amount Withdrawal successfull !");
+    }
+    else{
+        printf("PIN INVALID !!!");
+    }
+    
+}
+
+    else-if(ch == 2 && num == 002){
+        printf("Enter you PIN: ");
+    scanf("%d",&pin);
+    if(pin == num){
+        printf("Enter the amount: ");
+        scanf("%d",&amt);
+        printf("Please wait patiently....");
+        printf("Amount Withdrawal successfull !");
+    }
+    else{
+        printf("PIN INVALID !!!");
+    }
+    }
+    else-if(ch == 2 && num == 003){
+        printf("Enter you PIN: ");
+    scanf("%d",&pin);
+    if(pin == num){
+        printf("Enter the amount: ");
+        scanf("%d",&amt);
+        printf("Please wait patiently....");
+        printf("Amount Withdrawal successfull !");
+    }
+    else{
+        printf("PIN INVALID !!!");
+    }
+    }
+    else-if(ch == 2 && num == 004){
+        printf("Enter you PIN: ");
+    scanf("%d",&pin);
+    if(pin == num){
+        printf("Enter the amount: ");
+        scanf("%d",&amt);
+        printf("Please wait patiently....");
+        printf("Amount Withdrawal successfull !");
+    }
+    else{
+        printf("PIN INVALID !!!");
+    }
+    }
+    else-if(ch == 2 && num == 005){
+        printf("Enter you PIN: ");
+    scanf("%d",&pin);
+    if(pin == num){
+        printf("Enter the amount: ");
+        scanf("%d",&amt);
+        printf("Please wait patiently....");
+        printf("Amount Withdrawal successfull !");
+    }
+    else{
+        printf("PIN INVALID !!!");
+    }
+    }
+
+    else-if(ch == 3){
+        
+    }
+    
 
 
 
@@ -39,7 +138,7 @@ printf("*************************************\n");
 printf("\n=====================================\n");
 printf("   THANK YOU FOR USING OUR ATM        \n");
 printf("=====================================\n");
-printf("     Please collect your card         \n");
+
  
 
 
