f69175baf3640aee14fe6b9234141810832854dc
codex4jet <dipak01911@gmail.com>
2026-01-30 21:40:17 +0530

    added all ui text

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 int main(){
+    system("cls");
+    
+
+    // Welcome Screen ui 
+    printf("\n=====================================\n");
+printf("          WELCOME TO ATM             \n");
+printf("=====================================\n");
+printf("       Insert your ATM Card          \n");
+printf("        (Press Enter)                \n");
+
+
+    
+//  ATM main menu : UI
     printf("\n=====================================\n");
 printf("           ATM MAIN MENU              \n");
 printf("=====================================\n");
@@ -11,6 +26,22 @@ printf("5. Change PIN\n");
 printf("0. Exit\n");
 printf("-------------------------------------\n");
 printf("Enter your choice: ");
+
+
+
+// Success Message 
+printf("\n*************************************\n");
+printf("   TRANSACTION SUCCESSFUL             \n");
+printf("*************************************\n");
+
+// Exit screen 
+
+printf("\n=====================================\n");
+printf("   THANK YOU FOR USING OUR ATM        \n");
+printf("=====================================\n");
+printf("     Please collect your card         \n");
+ 
+
 
 
 
